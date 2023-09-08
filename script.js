99a6d7f371276aa71c863e9adde0728a863c3db5
lazaro <lazarosodre23@gmail.com>
2023-09-08 06:34:35 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+  const accordionItems = document.querySelectorAll('.accordion-item');
+
+  accordionItems.forEach(item => {
+    const header = item.querySelector('.accordion-header');
+    const content = item.querySelector('.accordion-content');
+
+    header.addEventListener('click', () => {
+      content.classList.toggle('active');
+    });
+  });
+});
+
